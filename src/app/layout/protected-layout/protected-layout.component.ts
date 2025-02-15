fa5739ae9294b9f598ab7e49e6974a279d610342
Sook Cheng <j.sookcheng@gmail.com>
2025-02-15 21:38:08 +0800

    update to angular 19 and added ng-bootstrap

--- src/app/layout/protected-layout/protected-layout.component.ts
+++ src/app/layout/protected-layout/protected-layout.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-protected-layout',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './protected-layout.component.html',
-  styleUrl: './protected-layout.component.less',
+    selector: 'app-protected-layout',
+    imports: [RouterOutlet],
+    templateUrl: './protected-layout.component.html',
+    styleUrl: './protected-layout.component.less'
 })
 export class ProtectedLayoutComponent {
 

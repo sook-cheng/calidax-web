0784149528aef7f3fa517a6741f5525458b8270e
Sook Cheng <j.sookcheng@gmail.com>
2025-03-11 12:29:16 +0800

    build files

--- dist/calidax/browser/chunk-HVNBVVPO.js
+++
@@ -1,1 +0,0 @@
-import{ca as t}from"./chunk-2ZDNKPQL.js";import"./chunk-QMQTSFHS.js";import"./chunk-FK6H3RFT.js";import"./chunk-EQDQRRRY.js";var p=[{path:"",pathMatch:"full",component:t}];export{p as PUBLIC_ROUTES};

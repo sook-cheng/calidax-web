ee82226279c1c2ad4cee39470649ba9691625bcc
Sook Cheng <j.sookcheng@gmail.com>
2025-04-10 16:30:26 +0800

    build files

---
+++ dist/calidax/server/chunk-QE77W4R2.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{Wa as t}from"./chunk-4RA2PHBA.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-5XO7ITTW.mjs";import"./chunk-S6KH3LOX.mjs";var p=[{path:"",pathMatch:"full",component:t}];export{p as PUBLIC_ROUTES};
